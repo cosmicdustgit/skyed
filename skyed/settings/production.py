61b00f8fd45e4d21c3ae4cf9d5159b00dfd259d4
cosmic <maildcos@gmail.com>
2016-05-28 21:28:03 +0000

    included image rendition in db.json

--- skyed/settings/production.py
+++ skyed/settings/production.py
@@ -1,35 +1,8 @@
 from .base import *
 
 DEBUG = False
-
-
-WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.wagtailsearch.backends.elasticsearch.ElasticSearch',
-        'INDEX': 'wagtaildemo'
-    }
-}
-
-
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379',
-        'KEY_PREFIX': 'wagtaildemo',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
-        }
-    }
-}
-
-# Use the cached template loader
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )),
-)
-
+SECRET_KEY = 'c(qefe16js8xm7*kzy#m*sorafo=^c9oe+zi$p4sq+1mw56765ufdgfp+n!efefef'
+ALLOWED_HOSTS=['skyed.pythonanywhere.com','localhost']
 
 try:
     from .local import *
